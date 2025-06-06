04d3fc4bccc62f135215bae75e316ba3e7e4d958
Nathanne Isip <nathanneisip@gmail.com>
2025-06-06 08:10:11 +0800

    Sorting direction enum definition for comparer.

---
+++ Glasgow/Glasgow.SortDirection.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Copyright 2025 Nathanne Isip
+ * This file is part of Glasgow (https://github.com/nthnn/Glasgow)
+ * This code is licensed under MIT license (see LICENSE for details)
+ */
+
+namespace Glasgow
+{
+    /// <summary>
+    /// Defines the direction in which a collection should be sorted.
+    /// </summary>
+    public enum SortDirection
+    {
+        /// <summary>
+        /// Sort items in ascending order (smallest to largest).
+        /// </summary>
+        Ascending,
+
+        /// <summary>
+        /// Sort items in descending order (largest to smallest).
+        /// </summary>
+        Descending
+    }
+}
